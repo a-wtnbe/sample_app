29cfe3e7481f4a12017ed1fbb34365f576c61dd4
a-wtnbe <ayawatanabe714@gmail.com>
2021-10-28 11:10:43 +0000

    gazo-ok

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad795703d69853e07d3fb9b7d9eb3670eccae62f39f77e6efcf7718d57114ff5d102d23a55ab755e106373fcf109844b0ba7b048a5b8a1ffc5abd2059eef900e'
